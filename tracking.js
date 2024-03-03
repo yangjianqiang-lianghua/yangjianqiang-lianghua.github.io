25cd3b533bf01e1530f56641d0cb47676ce2e94f
杨建强 <111231644+yangjianqiang-lianghua@users.noreply.github.com>
2024-03-03 15:39:45 +0800

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0a2b73b79f2c368bde489d729ef8e781";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
